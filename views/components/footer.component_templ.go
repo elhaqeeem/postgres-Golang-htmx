1010f9d722482140a837f30fde4431bc063aac86
Lukmanul Hakim <manglayangselalu@gmail.com>
2024-05-31 19:35:05 +0700

    update credential

--- views/components/footer.component_templ.go
+++ views/components/footer.component_templ.go
@@ -28,7 +28,7 @@ func Footer() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"absolute bottom-0 left-0 z-10 h-12 pl-4 text-center text-sm\"><a class=\"hover:text-sky-500 ease-in duration-300\" href=\"https://github.com/emarifer?tab=repositories\" target=\"_blank\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"absolute bottom-0 left-0 z-10 h-12 pl-4 text-center text-sm\"><a class=\"hover:text-sky-500 ease-in duration-300\" href=\"https://github.com/elhaqeeem?tab=repositories\" target=\"_blank\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
